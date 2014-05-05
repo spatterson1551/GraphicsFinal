d6725db9b825a9a0892d92be0ddd70585dd7b22e
DireLogomachist <milleram26@gmail.com>
2014-05-05 17:36:11 -0400

    Added base Intersection Test code
    Need to convert to add recursive calls and color returning.

---
+++ Program1/intersectTest.h
@@ -0,0 +1,19 @@
+#ifndef INTERSECTTEST_H
+#define INTERSECTTEST_H
+
+#include "../glm/glm.hpp"
+
+using namespace glm;
+
+//Need intersect
+
+double RaySphereIntersect(const vec3& P0, const vec3& V0, const mat4& T);
+//vec4 RaySphereIntersect(const vec3& P0, const vec3& V0, const mat4& T);
+
+double RayPolyIntersect(const vec3& P0, const vec3& V0, const vec3& p1, const vec3& p2, const vec3& p3, const mat4& T);
+//vec4 RayPolyIntersect(const vec3& P0, const vec3& V0, const vec3& p1, const vec3& p2, const vec3& p3, const mat4& T);
+
+double RayCubeIntersect(const vec3& P0, const vec3& V0, const mat4& T);
+//vec4 RayCubeIntersect(const vec3& P0, const vec3& V0, const mat4& T);
+
+#endif
